8391686dcc96c0e7d4ec5f9512b40df3685495cf
cs8433 <christian.hoerauf@consorsbank.de>
2019-11-05 17:44:07 +0100

    fizz is implemented

--- src/fizzbuzz/index.js
+++ src/fizzbuzz/index.js
@@ -1,4 +1,7 @@
 const fizzbuzz = (num) => {
+    if (num % 3 === 0) {
+        return 'fizz'
+    }
     return String(num)
 }
 module.exports = {
--- src/fizzbuzz/test.js
+++ src/fizzbuzz/test.js
@@ -7,4 +7,13 @@ describe('fizzbuzz', () => {
     it('should convert 2 to "2"', () => {
         expect(fizzbuzz(2)).toBe("2")
     })
+
+    it('should convert 3 to "fizz"', () => {
+        expect(fizzbuzz(3)).toBe("fizz")
+    })
+
+    it('should convert also 6 to "fizz"', () => {
+        expect(fizzbuzz(6)).toBe("fizz")
+    })
+
 })
